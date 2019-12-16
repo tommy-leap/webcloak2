9a34713eced29cd8906993c6bbbba0ab14937952
tommy-leap <tomohira07@icloud.com>
2019-12-16 17:11:58 +0900

    12/16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "ログインしました"
-      posts_path(resource)
+      posts_path
     end
   
   def after_sign_out_path_for(resource)
